eca66496b40114598c9ecd076641ecf23aed97d6
רבינוביץ שירה < @C23>
2024-06-17 18:41:57 +0300

    adding repos

---
+++ NodeJsProject/repositories/HelpRequestRepo.js
@@ -0,0 +1,13 @@
+//const BaseRepo = require('./BaseRepo');
+// import VolunteersModel from "../models/volunteersModel";
+import BaseRepo from "./BaseRepo.js";
+import HelpRequestModel from "../models/helpRequestModel.js";
+
+class HelpRequestRepo extends BaseRepo {
+    constructor(model) {
+        super(model);
+    }
+}
+
+//module.exports = new StudentRepo();
+export default new HelpRequestRepo(HelpRequestModel);
