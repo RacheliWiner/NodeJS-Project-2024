838b042e5003cf64470603a231b0819d5f703b57
RacheliWiner <r.96769@gmail.com>
2024-06-19 03:29:04 +0300

    starting pipeline

--- NodeJsProject/src/repositories/HelpRequestRepo.js
+++ NodeJsProject/src/repositories/HelpRequestRepo.js
@@ -1,5 +1,5 @@
 import connect from '../../config/db.js';
-import model from '../models/helpRequestModel.js';
+import model from '../models/HelpRequestModel.js';
 
 
 class HelpRequestRepo {
@@ -9,192 +9,89 @@ class HelpRequestRepo {
     connect();
   }
 
-  // async getAll(filters) {
-  //   try {
-  //     let query = {};
-  //     if(filters.location){
-  //         query.location = parseInt(filters.location);
-  //     }
-  //     if(filters.status){
-  //         query.status = filters.status;
-  //     }
-  //     if(filters.importance){
-  //         query.importance = filters.importance;
-  //     }
-  //     const ourPipeline = [
-  //       {
-  //         '$lookup': {
-  //           'from': 'location',
-  //           'localField': 'location',
-  //           'foreignField': '_id',
-  //           'as': 'location_info'
-  //         }
-  //       }, {
-  //         '$lookup': {
-  //           'from': 'status',
-  //           'localField': 'status',
-  //           'foreignField': '_id',
-  //           'as': 'status_info'
-  //         }
-  //       }, {
-  //         '$lookup': {
-  //           'from': 'priority',
-  //           'localField': 'importance',
-  //           'foreignField': '_id',
-  //           'as': 'priority_info'
-  //         }
-  //       }, {
-  //         '$unwind': {
-  //           'path': '$location_info'
-  //         }
-  //       }, {
-  //         '$unwind': {
-  //           'path': '$status_info'
-  //         }
-  //       }, {
-  //         '$unwind': {
-  //           'path': '$priority_info'
-  //         }
-  //       }, {
-  //         '$addFields': {
-  //           'city': '$location_info.city',
-  //           'street': '$location_info.street',
-  //           'importance': '$priority_info.description',
-  //           'status': '$status_info.description'
-  //         }
-  //       }, {
-  //         '$project': {
-  //           'location': 0,
-  //           'location_info': 0,
-  //           'status_info': 0,
-  //           'priority_info': 0
-  //         }
-  //       }
-  //     ]
-  //     console.log(query);
-  //     const pipeline = [];
-  //     pipeline.push({ $match: { status: "waiting" }})
-  //     Object.keys(query).length > 0?pipeline.push([{ $match: query }, {$match: {status:'waiting'}}]) : pipeline.push({ $match: { status: "waiting" }})
-  //     const results1 =  await this.model.aggregate(ourPipeline).exec();
-  //     const results2 =  await this.model.aggregate(pipeline).exec();
-  //     const combinedResults = [...results1, ...results2];
-  //     return combinedResults;
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //     throw error;
-  //   }
-  // }
 
 
 
-  //   async getAll() {
-  //     try {
-  //         const helpRequests = await HelpRequest.find({});
-  //         return helpRequests;
-  //     } catch (error) {
-  //         throw new Error('Error fetching help requests');
-  //     }
-  // }
-
-  async getAll() {
+  async getAll(filters) {
     try {
-        console.log("Attempting to fetch help requests");
-        const requests = await HelpRequest.find();
-        console.log("Successfully fetched help requests");
-        return requests;
+      const ourPipeline = []
+      // const ourPipeline = [
+      //   {
+      //     '$lookup': {
+      //       'from': 'location',
+      //       'localField': 'location',
+      //       'foreignField': '_id',
+      //       'as': 'location_info'
+      //     }
+      //   }, {
+      //     '$lookup': {
+      //       'from': 'status',
+      //       'localField': 'status',
+      //       'foreignField': '_id',
+      //       'as': 'status_info'
+      //     }
+      //   }, {
+      //     '$lookup': {
+      //       'from': 'priority',
+      //       'localField': 'importance',
+      //       'foreignField': '_id',
+      //       'as': 'priority_info'
+      //     }
+      //   }, {
+      //     '$unwind': {
+      //       'path': '$location_info'
+      //     }
+      //   }, {
+      //     '$unwind': {
+      //       'path': '$status_info'
+      //     }
+      //   }, {
+      //     '$unwind': {
+      //       'path': '$priority_info'
+      //     }
+      //   }, {
+      //     '$addFields': {
+      //       'city': '$location_info.city',
+      //       'street': '$location_info.street',
+      //       'importance': '$priority_info.description',
+      //       'status': '$status_info.description'
+      //     }
+      //   }, {
+      //     '$project': {
+      //       'location': 0,
+      //       'location_info': 0,
+      //       'status_info': 0,
+      //       'priority_info': 0
+      //     }
+      //   }
+      // ]
+      let query = {};
+      // if (filters.location) {
+      //   query.location = parseInt(filters.location);
+      // }
+      // if (filters.status) {
+      //   query.status = filters.status;
+      // }
+      // if (filters.importance) {
+      //   query.importance = filters.importance;
+      // }
+      // console.log(query);
+      // if (Object.keys(query).length > 0) {
+      //   ourPipeline.unshift({ $match: query });
+      // } else {
+      //   ourPipeline.push({ $match: { status: "waiting" } }, {$match: query });
+
+      // }
+      //const results = await this.model.aggregate(ourPipeline).exec();
+      
+      console.log(results);
+      return results;
     } catch (error) {
-        console.error("Error fetching help requests:", error);
-        throw new Error('Error fetching help requests');
+      console.error('Error fetching data:', error);
+      throw error;
     }
-}
-    // try {
-    //   const ourPipeline = [
-    //     {
-    //       '$lookup': {
-    //         'from': 'location',
-    //         'localField': 'location',
-    //         'foreignField': '_id',
-    //         'as': 'location_info'
-    //       }
-    //     }, {
-    //       '$lookup': {
-    //         'from': 'status',
-    //         'localField': 'status',
-    //         'foreignField': '_id',
-    //         'as': 'status_info'
-    //       }
-    //     }, {
-    //       '$lookup': {
-    //         'from': 'priority',
-    //         'localField': 'importance',
-    //         'foreignField': '_id',
-    //         'as': 'priority_info'
-    //       }
-    //     }, {
-    //       '$unwind': {
-    //         'path': '$location_info'
-    //       }
-    //     }, {
-    //       '$unwind': {
-    //         'path': '$status_info'
-    //       }
-    //     }, {
-    //       '$unwind': {
-    //         'path': '$priority_info'
-    //       }
-    //     }, {
-    //       '$addFields': {
-    //         'city': '$location_info.city',
-    //         'street': '$location_info.street',
-    //         'importance': '$priority_info.description',
-    //         'status': '$status_info.description'
-    //       }
-    //     }, {
-    //       '$project': {
-    //         'location': 0,
-    //         'location_info': 0,
-    //         'status_info': 0,
-    //         'priority_info': 0
-    //       }
-    //     }
-    //   ]
-    //   let query = {};
-    //   if (filters.location) {
-    //     query.location = parseInt(filters.location);
-    //   }
-    //   if (filters.status) {
-    //     query.status = filters.status;
-    //   }
-    //   if (filters.importance) {
-    //     query.importance = filters.importance;
-    //   }
-    //   console.log(query);
-    //   if (Object.keys(query).length > 0) {
-    //     ourPipeline.unshift({ $match: query });
-    //   } else {
-    //     ourPipeline.push({ $match: { status: "waiting" } }, {$match: query });
-
-    //   }
-    //   const results = await this.model.aggregate(ourPipeline).exec();
-    //   console.log(results);
-    //   return results;
-    // } catch (error) {
-    //   console.error('Error fetching data:', error);
-    //   throw error;
-    // }
-
-  //     const pipeline = [];
-  //     Object.keys(query).length > 0 ? pipeline.push({ $match: query }) : pipeline.push({ $match: { status: "W" } })
-  //     ourPipeline.push(pipeline);
-  //     const results = await this.model.aggregate(ourPipeline).exec();
-  //     console.log(results);
-  //     return results;
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //     throw error;
-  //   }
-  // }
-
+  }
+ 
   async getById(id) {
     try {
       let item = await this.model.findById(Number(id));
--- NodeJsProject/src/routers/HelpRequestRouter.js
+++ NodeJsProject/src/routers/HelpRequestRouter.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import helpRequestController from '../controllers/HelpRequestsController.js';
+import helpController from '../controllers/HelpRequestsController.js';
 const router = express.Router();
-router.get('/', (req, res, next) => helpRequestController.getAll(req, res, next));
-router.get('/:id', helpRequestController.getById);
-// router.get('/', helpController.getAll);
-router.put('/:id', helpRequestController.update);
+router.get('/:id', helpController.getById);
+router.get('/', helpController.getAll);
+router.put('/:id', helpController.update);
 
 
 export default router;
